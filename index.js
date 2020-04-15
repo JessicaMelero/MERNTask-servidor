f589c20ffb069d3d3fac64571d48f1f0b140a6b4
jessica <jessicameleromartinez@gmail.com>
2020-04-15 19:09:48 +0200

    conectado mongo a la app

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const conectarDB = require('./config/db.js');
 
 // Crear el servidor
 const app = express();
+
+// Conectar a la db
+conectarDB();
 
 // Puerto de la app
 const PORT = process.env.PORT || 4000;
