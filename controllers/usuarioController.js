579cf239e1fbe63da7620e5559d2b6ba81d4fb05
jessica <jessicameleromartinez@gmail.com>
2020-04-15 20:07:04 +0200

    Envio de peticion + lectura de body

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,5 @@
 
 
 exports.crearUsuario = (req, res) => {
-  console.log("Desde crear usuario");
+  console.log(req.body);
 }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,10 +3,9 @@ const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 
-// Crea un usuario
-// api/usuarios
+// Crea un usuario api/usuarios
 router.post('/',
   usuarioController.crearUsuario
-)
+);
 
 module.exports = router;
