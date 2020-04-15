101d6031204ba9f7584587369e3c054781bf03ef
jessica <jessicameleromartinez@gmail.com>
2020-04-15 19:18:07 +0200

    Configuracion rutas de usuarios

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ conectarDB();
 // Puerto de la app
 const PORT = process.env.PORT || 4000;
 
+// Importar Rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+
 // Arrancar server
 app.listen(PORT, () =>{
   console.log("Servidor funcionando en el puerto "+PORT);
