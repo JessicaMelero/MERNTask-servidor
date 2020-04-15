7dc8f3ac6af9f859a15071b0a3985ed957233d47
jessica <jessicameleromartinez@gmail.com>
2020-04-15 18:25:41 +0200

    Creado servidor de Express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Desde index.js");
+const express = require('express');
+
+// Crear el servidor
+const app = express();
+
+// Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// Arrancar server
+app.listen(PORT, () =>{
+  console.log("Servidor funcionando en el puerto "+PORT);
+})
