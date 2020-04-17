1be070ac236a33b777cede80f1fc94a22205d7dc
jessica <jessicameleromartinez@gmail.com>
2020-04-17 17:49:49 +0200

    Ruta para nuevos proyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,3 +1,14 @@
-exports.crearProyecto = async(req,res) => {
+const Proyecto = require('../models/Proyecto');
 
+exports.crearProyecto = async(req,res) => {
+  try {
+    // Crear proyecto
+    const proyecto = new Proyecto(req.body);
+    proyecto.save();
+    res.json(proyecto);
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Hubo un error');
+  }
 }
