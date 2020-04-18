9b81245ee9a9da1d5c71e845fe6c9178ec106deb
jessica <jessicameleromartinez@gmail.com>
2020-04-18 16:19:53 +0200

    Obteniendo info de usuario autenticado

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 require('dotenv').config({path: 'variables.env'});
 
 const conectarDB = async () => {
-  
+
   try {
     await mongoose.connect(process.env.DB_MONGO,{
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false
+      useFindAndModify: false,
     });
     console.log("Conectada a la base de datos correctamente");
   } catch (e) {
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const {check} = require('express-validator');
 const authController = require('../controllers/authController');
+const auth = require('../middleware/auth');
 
 // Crea un usuario api/auth
 router.post('/',
@@ -11,6 +12,11 @@ router.post('/',
     check("password", "El password debe tener al menos 6 caracteres").isLength({min: 6})
   ],
   authController.autenticarUsuario
+);
+// Obtiene usuario autenticado
+router.get('/',
+  auth,
+  authController.usuarioAutenticado
 );
 
 module.exports = router;
